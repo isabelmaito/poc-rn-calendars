d4efda2ee50540e101546c1a26147fd5f4d92543
lucas consani <lucas_consani1@hotmail.com>
2025-09-01 16:40:42 -0300

    header
    header e barra de busca implementado

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Header from "./src/screens/Header";
 import HomeScreen from "./src/screens/HomeScreen";
 import ResultsScreen from "./src/screens/ResultsScreen";
 
@@ -9,10 +11,28 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Results" component={ResultsScreen} />
-      </Stack.Navigator>
+      <View style={styles.container}>
+        {/* Header fixo em todas as telas */}
+        <Header />
+
+        {/* Navegação das páginas */}
+        <View style={styles.content}>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Results" component={ResultsScreen} />
+          </Stack.Navigator>
+        </View>
+      </View>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    flex: 1,
+  },
+});
